cb0515dff23beb5eeb9c80c65dd1c7270e2d472d
Dmitrii Kulik <djvudakulik@gmail.com>
2023-03-16 13:57:45 +0200

    Early stop.

--- trainer.py
+++ trainer.py
@@ -13,7 +13,6 @@ from torch.distributed import broadcast_object_list
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-
 from .config import Config
 from .distributed import (
     global_leader_only,
@@ -31,6 +30,71 @@ _command: str
 
 # output dynamic data
 _writer: Optional[SummaryWriter] = None
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+
+    def __init__(self, patience=7, verbose=False, delta=0, path: Optional[str] = 'checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+
+    def __call__(self, val_loss, model: Optional = None, save_fn: Optional = None):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            if model:
+                self.save_checkpoint(val_loss, model)
+            elif save_fn is not None:
+                save_fn()
+
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            if model:
+                self.save_checkpoint(val_loss, model)
+            elif save_fn is not None:
+                save_fn()
+
+            self.counter = 0
+
+        return self.early_stop
+
+    def save_checkpoint(self, val_loss, model):
+        """Saves model when validation loss decrease."""
+        if self.verbose:
+            self.trace_func(
+                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
+
+_stopper = EarlyStopping(patience=10)
 
 
 def get_global_step():
@@ -216,13 +280,25 @@ def train(
             if cfg.save_on_quit:
                 saving_commands.append("quit")
 
-            if engines.global_step % save_ckpt_every == 0 or command in saving_commands:
-                engines.save_checkpoint()
+            if engines.global_step != 0 and engines.global_step % save_ckpt_every == 0 or command in saving_commands:
+                engines.save_checkpoint(tag=None)
 
             if engines.global_step % cfg.eval_every == 0 or command in ["eval"]:
                 engines.eval()
-                eval_fn(engines=engines)
+                stats = eval_fn(engines=engines)
+                # it's the easiest way to show progress of the model
+                if _writer is not None:
+                    for k, v in stats.items():
+                        if isinstance(v, str):
+                            continue
+
+                        _writer.add_scalar(f"{get_cfg().model}/val/{k}", v, global_step=engines.global_step)
+
                 engines.train()
+
+                if _stopper(stats["val_loss"],
+                            save_fn=lambda: engines.save_checkpoint(tag="best")):
+                    return
 
             if command in ["quit"]:
                 return
